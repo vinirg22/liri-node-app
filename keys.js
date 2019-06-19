27991c61fa32d8d14d99d1600a1be76096a19475
vinirg22 <aramburo.irving@gmail.com>
2019-06-19 10:02:57 -0700

    Added function for all different commands. Also, a switch statement to help it run code based on cases. Finally, got my commands to work, but the do-what-it-says section is still in progress

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: a142d0efe54a4e4e9c68460913ebce9f,
-  secret: a16f40ada75b45b3ace17c88393c3059
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 //  spotify secret key: 1b0e148c00a443acb58b3ffbebe9152e
--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var keys = require("./keys");
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 // var dotenv = require("")
 
 var spotify = new Spotify(keys.spotify);
 
-var command = process.argv[2];
-var input = process.argv.slice(3).join(" ");
+// var command = process.argv[2];
+// var input = process.argv.slice(3).join(" ");
 
 
 // node liri.js concert-this <artist/band name here>
-if (command === "concert-this") {
+var concertThis = function (artist) {
   console.log("running concert this")
   // This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
 
@@ -23,45 +23,71 @@ if (command === "concert-this") {
     console.log(response.data);
   });
 }
-  // node liri.js spotify-this-song '<song name here>'
-if (command === "spotify-this-song"); {
-  // console.log("running spotify-this-song")
+// node liri.js spotify-this-song '<song name here>'
+// if (command === "spotify-this-song"); {
+// console.log("running spotify-this-song")
+var getArtists = function (artist) {
+  return artist.name;
 
-  //    This will show the following information about the song in your terminal/bash window
-  spotify.search({ type: 'track', query: input }, function (err, data) {
+}
+//    This will show the following information about the song in your terminal/bash window
+var spotifySearch = function (songName) {
+  console.log(songName);
+  spotify.search({ type: "track", query: songName }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    const items = data.tracks.items;
-
-    for (let i = 0; i < items.lenght; i++) {
-      const songTitle = items[i].name;
-      const songPreviewUrl = items[i].preview_url;
-      const artistName = items[i].album.artis[0].name;
+    var items = data.tracks.items;
+    // console.log(items);
+    for (var i = 0; i < items.length; i++) {
+      var songTitle = items[i].name;
+      var songPreviewUrl = items[i].preview_url;
+      var artistName = items[i].artists.map(getArtists);
+      var albumName = items[i].album.name;
       console.log(artistName + " - " + songTitle);
       console.log("Preview URL: " + songPreviewUrl);
+      console.log("albumName " + albumName); 
       console.log("------------");
     }
   });
 }
+// }
 // node liri.js movie-this '<movie name here>'
-if (command === "movie-this") {
+var movieThis = function (movieName) {
   axios.get("http://www.omdbapi.com/?t=" + input + "&y=plot=short&apikey=" + keys.omdbKey.apiKey)
-  .then(function (response) {
-    const movie = response.data;
-    console.log("Movie Title: " + movie.Title);
-    console.log("Actors: " + movie.Actors);
-    console.log("Rotten Tomatoes Score: " + movier.Ratings[1].value);
-  })
-  .catch(function (err) {
-     console.log(err);
-  });
+    .then(function (response) {
+      const movie = response.data;
+      console.log("Movie Title: " + movie.Title);
+      console.log("Actors: " + movie.Actors);
+      console.log("Rotten Tomatoes Score: " + movier.Ratings[1].value);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
-
 // node liri.js do-what-it-says
+// if (command === "do-what-it-says") {
 
-
-
+// }
+var select = function (type, input) {
+  switch (type) {
+    case "concert-this":
+      concertThis(inpout);
+      break;
+    case "spotify-this-song":
+      spotifySearch(input);
+      break;
+    case "movie-this":
+      movieThis(input);
+      break;
+    default: 
+    console.log("inputErr");
+  }
+}
+var start = function (type, input) {
+  select(type, input);
+}
+start (process.argv[2], process.argv[3]);
 
 
 
