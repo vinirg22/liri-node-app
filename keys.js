6fd2ac16dc20d338a59830dbbf07637f3d43b9cc
vinirg22 <aramburo.irving@gmail.com>
2019-06-18 21:04:18 -0700

    Concert-this command added. spotify this song set up in process

--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: a142d0efe54a4e4e9c68460913ebce9f,
+  secret: a16f40ada75b45b3ace17c88393c3059
 };
+//  spotify secret key: 1b0e148c00a443acb58b3ffbebe9152e
--- liri.js
+++ liri.js
@@ -3,32 +3,60 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
-// var dotenv = reuire("")
+// var dotenv = require("")
 
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
-var artist = process.argv[3];
+var input = process.argv.slice(3).join(" ");
 
 
 // node liri.js concert-this <artist/band name here>
 if (command === "concert-this") {
-    console.log("running concert this")
-    // This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+  console.log("running concert this")
+  // This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
 
-    // Name of the venue
-    // Venue location
-    // Date of the Event (use moment to format this as "MM/DD/YYYY")
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-
-    axios.get(queryUrl).then(function (response) {
-        console.log(response.data);
-    });
-} else ()
+  // Name of the venue
+  // Venue location
+  // Date of the Event (use moment to format this as "MM/DD/YYYY")
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
 
-// node liri.js spotify-this-song '<song name here>'
+  axios.get(queryUrl).then(function (response) {
+    console.log(response.data);
+  });
+}
+if (command === "spotify-this-song"); {
+  // console.log("running spotify-this-song")
+  // node liri.js spotify-this-song '<song name here>'
+  //    This will show the following information about the song in your terminal/bash window
+
+  spotify.search({ type: 'track', query: input }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    // Artist(s)
+    // The song's name
+    // A preview link of the song from Spotify
+    // The album that the song is from
+    const items = data.tracks.items;
+
+    for (let i = 0; i < items.lenght; i++) {
+      const songTitle = items[i].name;
+      const songPreviewUrl = items[i].preview_url;
+      const artistName = items[i].album.artis[0].name;
+      console.log(artistName + " - " + songTitle);
+      console.log("Preview URL: " + songPreviewUrl);
+      console.log("------------");
+    }
+  });
+}
+
+
+
+
+
+
 
 // node liri.js movie-this '<movie name here>'
 
